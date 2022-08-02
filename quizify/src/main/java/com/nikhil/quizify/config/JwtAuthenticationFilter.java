15e18dc9797c550ed582911fc62497c764bcfe05
Nikhil <nsoni19012001@gmail.com>
2022-08-03 02:11:26 +0530

    Spring security added.

---
+++ quizify/src/main/java/com/nikhil/quizify/config/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.nikhil.quizify.config;
+
+import com.nikhil.quizify.service.impl.UserDetailsServiceImpl;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        final String reqToken = httpServletRequest.getHeader("Authorization");
+        String username = null, jwtToken = null;
+
+        // Validating token
+        if (reqToken != null && reqToken.startsWith("Bearer ")) {
+            jwtToken = reqToken.substring(7);
+            try {
+                username = this.jwtUtils.extractUsername(jwtToken);
+            } catch (ExpiredJwtException e) {
+                e.printStackTrace();
+                System.out.println("Token Expired!!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Some error occurred!!");
+            }
+        } else {
+            logger.error("Invalid Token!!");
+        }
+
+        // Validated
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            final UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if (this.jwtUtils.validateToken(jwtToken, userDetails)) {
+                //valid
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        } else {
+            System.out.println("Token is invalid!");
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
