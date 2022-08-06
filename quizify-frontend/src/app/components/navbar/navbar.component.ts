ade9345664ee3aacd368f19f35786ed58ebd7d3a
Nikhil <nsoni19012001@gmail.com>
2022-08-07 00:00:24 +0530

    Fix Login page

--- quizify-frontend/src/app/components/navbar/navbar.component.ts
+++ quizify-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn = false;
+  user = null;
+  constructor(public loginService: LoginService) {}
 
   ngOnInit(): void {
+    this.isLoggedIn = this.loginService.isLoggedIn();
+    this.user = this.loginService.getUser();
+    this.loginService.loginStatusSubject.asObservable().subscribe((data) => {
+      this.isLoggedIn = this.loginService.isLoggedIn();
+      this.user = this.loginService.getUser();
+    });
   }
 
+  logout() {
+    this.loginService.logout();
+    // this.isLoggedIn = false;
+    // this.user = null;
+    // window.location.reload();
+  }
 }
